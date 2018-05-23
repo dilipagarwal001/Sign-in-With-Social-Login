3e4b522b447d3a75aafc32fe2cabcce479b6627e
Dilip Agarwal <37178070+dilipagarwal001@users.noreply.github.com>
2018-05-23 22:48:17 +0530

    Update settings.php

--- instagram/settings.php
+++ instagram/settings.php
@@ -1,12 +1,12 @@
 <?php
 
 /* Instagram App Client Id */
-define('CLIENT_ID','ba9b55d4f37145f4827e7c0f9d063011');
+define('CLIENT_ID','xxxx'); //put your client id here
 
 /* Instagram App Client Secret */
-define('CLIENT_SECRET','18b22770ac404008accc4f1aa42590e4');
+define('CLIENT_SECRET','xxxx'); //put your Client Secret here
 
 /* Instagram App Redirect Url */
-define('CLIENT_REDIRECT_URL','https://github.com/dilipagarwal001/Sign-in-With-Social-Login/blob/master/instagram/instacallback.php');
+define('CLIENT_REDIRECT_URL','http://localhost/instagram/instacallback.php');
 
 ?>
