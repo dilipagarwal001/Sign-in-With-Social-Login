e07d74d837b4c28468a7c950b2807aa0e678a8a7
Dilip Agarwal <37178070+dilipagarwal001@users.noreply.github.com>
2018-05-23 22:46:08 +0530

    Update settings.php

--- instagram/settings.php
+++ instagram/settings.php
@@ -7,6 +7,6 @@ define('CLIENT_ID','ba9b55d4f37145f4827e7c0f9d063011');
 define('CLIENT_SECRET','18b22770ac404008accc4f1aa42590e4');
 
 /* Instagram App Redirect Url */
-define('CLIENT_REDIRECT_URL','http://localhost/projects/SocialLoginsSignIn/instagram/instacallback.php');
+define('CLIENT_REDIRECT_URL','https://github.com/dilipagarwal001/Sign-in-With-Social-Login/blob/master/instagram/instacallback.php');
 
 ?>
